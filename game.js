beeacf4af63e5a2b14370c02ce0f343069704d04
Matt Mahowald <10021147+mattmahowald@users.noreply.github.com>
2022-05-24 03:13:10 +0000

    up

--- game.js
+++ game.js
@@ -8,6 +8,9 @@ var State = {
 var gameState = State.INIT_INTRO;
 
 var objectMap;
+var rotationSpeed =  0.2;
+var speed = 0;
+
 
 //learn about Object.keys() here: https://stackoverflow.com/questions/5223/length-of-a-javascript-object
                                 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
@@ -20,32 +23,73 @@ var clickableObjects = [];
 var clickCallbackFunctions = [];
 
 var MOVE_SPEED = 2;
+var meteorVX = 8;
+var meteorVY = 4;
 
-function playGame(){
-   
+function playGame(){  
+    moveShip();
+    collide();
+    moveMeteor();
+    moveMeteor2();
 }
-function start(){
-    
+// function math(){
+//     if( collide == true){
+//         var firstNum = Randomizer.nextInt(0,50);
+//         var secondNum = Randomizer.nextInt(0,50);
+        
+//     }
+// }
+function start() 
+{
+    mouseClickMethod(clickHandler); //clickHandler will be called when the mouse is clicked
+    setBackgroundColor(Color.BLACK);
+    loadImages();
 }
 
+
+function collide (){
+    console.log(objectMap.meteor);
+    var shipX = objectMap.ship.getX();
+    var shipY = objectMap.ship.getY();
+    if(objectMap.meteor.containsPoint(shipX, shipY))
+    {
+        console.log("middle");
+        crash();
+    }
+}
+function crash(){
+    remove(objectMap.meteor);
+    remove(objectMap.meteor2)
+    remove(objectMap.ship)
+    add(objectMap.boom);
+    math;
+}
 
 //loads all of the web images in objectMap
 function loadImages()
 {
     objectMap = {
         title: new WebImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmpefmp_9mjNYZo60rUr4hDUtcdXKL7_1akA&usqp=CAU"),
-        ship: new WebImage("https://www.clipartmax.com/png/middle/2-20463_cartoon-rocket-ship-clipart-rocket-clipart.png"),
-        playButton : new WebImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAMdpXAZ4d7VJRDC3RUo_v6qT6vCU1ESJ2hw&usqp=CAU")
-    }
+        ship: new WebImage("https://cdn.pixabay.com/photo/2017/06/25/22/00/rocket-2442125_960_720.png"),
+        playButton : new WebImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyxquXU-SY5qNkXhtg6YK61Uabi_qttEm-Iw&usqp=CAU"),
+        meteor  : new WebImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx3BaMusudcsynKw7KLM0QM1WXa56rGeCOXw&usqp=CAU"),
+        meteor2  : new WebImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx3BaMusudcsynKw7KLM0QM1WXa56rGeCOXw&usqp=CAU"),
+ 
+      boom : new WebImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2X0qQIap8M5N2a9TfWbcSHwEMnEk_XXPFUQ&usqp=CAU")
 
+        
+    }
+    objectMap.ship.setSize(getWidth()/20, getHeight()/10)
+
+    objectMap.boom.setPosition(100, 100);
+    objectMap.meteor.setSize(getWidth()/5,getHeight() / 5)
     objectMap.title.loaded(loadedCallback);
     objectMap.ship.loaded(loadedCallback);
     objectMap.playButton.loaded(loadedCallback);
     
-    
     console.log("Loading Images . . .");
     loaded = true;
-    setTimer(gameManager, 20);
+    setTimer(gameManager, 50);
 }
 
 //called when a WebImage is loaded. If all the images are loaded, it will start the game
@@ -58,7 +102,6 @@ function loadedCallback()
     {
         console.log("Load Success!");
         loaded = true;
-        setTimer(gameManager, 20);
     }
 }
 
@@ -87,7 +130,6 @@ function gameManager()
 //initialize all of the objects used in the intro and add them onto the sceen
 function initIntro()
 {
-    console.log("Init intro");
     removeAll();
     
     setPositionCentered(objectMap.title, getWidth() / 2, -100);
@@ -99,12 +141,22 @@ function initIntro()
     addClickableObject(
         objectMap.playButton, 
         function() { 
+            remove(objectMap.playButton);
+            remove(objectMap.title);
             gameState = State.PLAY;
+            objectMap.ship.setPosition(
+              (getWidth() - objectMap.ship.getWidth()) / 2,
+              (getHeight() - objectMap.ship.getHeight()) / 2,
+            )
+            add(objectMap.meteor);
+            add(objectMap.ship);
         }
     );
     
     gameState = State.PLAY_INTRO;
 }
+
+
 
 //move the game title and play button onto the screen
 function playIntro()
@@ -122,15 +174,67 @@ function playIntro()
     }
 }
 
+function moveMeteor() {
+  objectMap.meteor.move(meteorVX,meteorVY);
+  var meteorX = objectMap.meteor.getX();
+  var meteorY = objectMap.meteor.getY();
+  var meteorWidth = objectMap.meteor.getWidth();
+  var meteorHeight = objectMap.meteor.getHeight();
+  
+  if(meteorX < 0 || meteorX + meteorWidth > getWidth()) {
+      meteorVX = -meteorVX;
+  }
+  if (meteorY < 0 || meteorY + meteorHeight > getHeight()) {
+      meteorVY = -meteorVY;
+  }
+}
+
+function moveMeteor2() {
+  objectMap.meteor2.move(meteorVX,meteorVY);
+  var meteorX = objectMap.meteor2.getX();
+  var meteorY = objectMap.meteor2.getY();
+  var meteorWidth = objectMap.meteor2.getWidth();
+  var meteorHeight = objectMap.meteor2.getHeight();
+  
+  if(meteorX < 0 || meteorX + meteorWidth > getWidth()) {
+      meteorVX = -meteorVX;
+  }
+  if (meteorY < 0 || meteorY + meteorHeight > getHeight()) {
+      meteorVY = -meteorVY;
+  }
+}
+
 function moveShip()
 {
-    var moveX = 0;
-    var moveY = 0;
+    var theta = objectMap.ship.rotation;
+    if(isKeyPressed(Keyboard.LEFT)) rotationSpeed-= 5;
+    if(isKeyPressed(Keyboard.RIGHT)) rotationSpeed+=5;
+    if(isKeyPressed(Keyboard.UP)) speed -= MOVE_SPEED;
+    if(isKeyPressed(Keyboard.DOWN)) speed += MOVE_SPEED;
+
+    var shipX = objectMap.ship.getX();
+    var shipY = objectMap.ship.getY();
+    var shipWidth = objectMap.ship.getWidth();
+    var shipHeight = objectMap.ship.getHeight();
+
+    var dx = -Math.sin(theta) * speed;
+    var dy = Math.cos(theta) * speed;
     
-    if(isKeyPressed(Keyboard.LEFT)) moveX -= MOVE_SPEED;
-    if(isKeyPressed(Keyboard.RIGHT)) moveX += MOVE_SPEED;
-    if(isKeyPressed(Keyboard.UP)) moveY -= MOVE_SPEED;
-    if(isKeyPressed(Keyboard.DOWN)) moveY += MOVE_SPEED;
+    if(shipX < 0 && shipX && dx < 0) {
+        dx = 0;
+    }
+    if (shipX + shipWidth > getWidth() && dx > 0) {
+        dx = 0;
+    }
+    if(shipY < 0 && dy < 0){
+        dy = 0;
+    }
+    if (shipY + shipHeight > getHeight() && dy > 0) {
+        dy = 0;
+    }
+    
+    objectMap.ship.move(dx, dy);
+    objectMap.ship.setRotation(theta + rotationSpeed);
 }
 
 //make no objects clickable
